0cf58cfb6d64157191fc765064097536aef68eae
morgan-cobb <morgancobb3@gmail.com>
2024-10-07 21:14:28 +0100

    Added logic for game controller and keyboard

--- firmware/controller/keyboard.cpp
+++ firmware/controller/keyboard.cpp
@@ -6,7 +6,6 @@
 #include <linux/input.h>
 #include <fcntl.h>
 #include <unistd.h>
-using namespace std;
 
 #define SONAR3 22
 
@@ -100,7 +99,7 @@ int main()
     dma_handler dma_object = dma_handler(125, 250, 0, 26);
 
     // Controller input setup
-    const char* device_path = "/dev/input/event4"; // Adjust path as necessary for your Xbox controller
+    const char* device_path = "/dev/input/event4";
     int fd = open(device_path, O_RDONLY);
     if (fd == -1) {
         std::cerr << "Failed to open input device: " << device_path << std::endl;
@@ -108,7 +107,6 @@ int main()
     }
 
     struct input_event ev;
-    uint8_t power = 175; // Default power level for motors
     while (true) {
         ssize_t bytes = read(fd, &ev, sizeof(ev));
         if (bytes < (ssize_t)sizeof(ev)) {
@@ -119,41 +117,21 @@ int main()
         if (ev.type == EV_KEY) {
             if (ev.value == 1) { // Key pressed
                 switch (ev.code) {
-                    case BTN_A: // A button: Move forward
-                        move_forward(power, dma_object);
-                        cout << "Button A pressed: Moving forward" << endl;
+                    case KEY_W: // Forward
+                        move_forward(175, dma_object);
                         break;
-                    case BTN_B: // B button: Move backward
-                        move_backward(power, dma_object);
-                        cout << "Button B pressed: Moving backward" << endl;
+                    case KEY_S: // Backward
+                        move_backward(175, dma_object);
                         break;
-                    case BTN_X: // X button: Turn left
-                        turn_left(power, dma_object);
-                        cout << "Button X pressed: Moving left" << endl;
+                    case KEY_A: // Left
+                        turn_left(175, dma_object);
                         break;
-                    case BTN_Y: // Y button: Turn right
-                        turn_right(power, dma_object);
-                        cout << "Button Y pressed: Moving right" << endl;
+                    case KEY_D: // Right
+                        turn_right(175, dma_object);
                         break;
                 }
             } else if (ev.value == 0) { // Key released
                 dma_object.turn_off();
-            }
-        } else if (ev.type == EV_ABS) {
-            // Handle joystick movement for turning
-            if (ev.code == ABS_X) {
-                if (ev.value < 128) {
-                    // Joystick moved left
-                    turn_left(power, dma_object);
-                    cout << "Joystick moved left: Moving left" << endl;
-                } else if (ev.value > 128) {
-                    // Joystick moved right
-                    turn_right(power, dma_object);
-                    cout << "Joystick moved right: Moving right" << endl;
-                } else {
-                    // Joystick centered
-                    dma_object.turn_off();
-                }
             }
         }
     }
